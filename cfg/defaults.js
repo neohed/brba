84d90f237ca04463073a68787a54103e5b83ab5e
neohed <dave@neohed.com>
2019-10-26 23:23:47 +0100

    Removed React packages and config

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -11,7 +11,7 @@ const getCommonModules = () => ({
             use: {
                 loader: "babel-loader",
                 query: {
-                    "presets": ["@babel/preset-env", "@babel/preset-react"]
+                    "presets": ["@babel/preset-env"]
                 }
             }
         }, {
